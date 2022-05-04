6194d3506acef23db89345cdfe727e6af1c465d8
Resi Respati <resir014@gmail.com>
2022-05-04 21:23:00 +0700

    feat: use typekit fonts, homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import '~/styles/fonts.css';
 import '~/styles/globals.css';
 
 const queryClient = new QueryClient();
@@ -13,6 +12,7 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <QueryClientProvider client={queryClient}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://use.typekit.net/jby4txw.css" />
       </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
