ec5b6533890d30231417d8649975c227876ed64a
Resi Respati <resir014@gmail.com>
2022-05-04 20:53:33 +0700

    add more scenes

--- src/modules/matches/hooks.ts
+++ src/modules/matches/hooks.ts
@@ -1,8 +1,13 @@
 import { useQuery } from 'react-query';
 import { getNextMatch } from './api';
 
+export interface PlayerDetails {
+  country: string;
+  name: string;
+}
+
 export function useNextMatch(refetchInterval = 10000) {
-  const query = useQuery('next-matches', getNextMatch, { refetchInterval });
+  const query = useQuery<PlayerDetails[]>('next-matches', getNextMatch, { refetchInterval });
 
   return query;
 }
