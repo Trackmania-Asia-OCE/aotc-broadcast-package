01bb31d11ef8f71a79f6c590adacb1b02d22e0b6
Resi Respati <resir014@gmail.com>
2023-07-22 13:43:21 +0700

    add results + next match store

---
+++ src/modules/matches/next-match-store.ts
@@ -0,0 +1,31 @@
+import { produce } from 'immer';
+import { create } from 'zustand';
+import { PlayerData } from '~/server/types';
+
+export interface NextMatchStore {
+  players: (PlayerData | undefined)[];
+  setPlayerAtIndex?: (index: number, player?: PlayerData) => void;
+  removePlayerAtIndex?: (index: number) => void;
+}
+
+const defaultPlayers = Array<PlayerData | undefined>(4).fill(undefined);
+
+export const useNextMatchStore = create<NextMatchStore>(set => ({
+  players: defaultPlayers,
+  setPlayerAtIndex(index, player) {
+    return set(
+      produce((state: NextMatchStore) => {
+        state.players[index] = player;
+      })
+    );
+  },
+  removePlayerAtIndex(index) {
+    return set(
+      produce((state: NextMatchStore) => {
+        if (index < state.players.length) {
+          state.players[index] = undefined;
+        }
+      })
+    );
+  },
+}));
